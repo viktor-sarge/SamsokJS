a7200a9eaf24eee773f42249405dd8256f79393c
Danni Efraim <danni@compiled.se>
2014-11-20 14:13:43 +0100

    SSB provider.

--- js/parsers.js
+++ js/parsers.js
@@ -1,5 +1,26 @@
 var searchIdCounter = 0;
 
+function loadXml(xmlStr) {
+    var parser, xmlDoc;
+    if(window.DOMParser) {
+        parser = new DOMParser();
+        xmlDoc = parser.parseFromString(xmlStr, "text/xml");
+    } else {
+        xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
+        xmlDoc.async=false;
+        xmlDoc.loadXML(xmlStr);
+    }
+    return xmlDoc;
+}
+
+function xpathString(node, expr) {
+    return document.evaluate(expr, node, null, XPathResult.STRING_TYPE, null).stringValue;
+}
+
+function xpathNodes(node, expr) {
+    return document.evaluate(expr, node, null, XPathResult.ANY_TYPE, null);
+}
+
 App.SearchHit = Ember.Object.extend({
     title: null,
     author: null,
@@ -42,6 +63,31 @@ App.SsbParser = Ember.Object.extend({
 
     getHits: function(content, baseurl) {
         var hits = [];
+            html = loadXml(content);
+
+        var totalHits = xpathString(html, "//div[@id='results-filter']/p[@class='total']/em[3]");
+
+        this.set('totalHits', totalHits);
+
+        var results = xpathNodes(html, "//ol[@class='results-icon']//div[string(number(@id)) != 'NaN' and @class='row-fluid']");
+        var result = results.iterateNext();
+        while (result) {
+            var title = xpathString(result, ".//div[@class='title']/h2");
+            var author = xpathString(result, ".//span[@class='author']");
+            var type = xpathString(result, ".//span[@class='mediatype']");
+            var year = xpathString(result, ".//span[@class='year']");
+            var url = baseurl + xpathString(result, ".//div[@class='title']/h2/a/@href");
+            hits.push(
+                App.SearchHit.create({
+                    title: title,
+                    author: author,
+                    type: type,
+                    year: year,
+                    url: url
+                })
+            );
+            result = results.iterateNext();
+        }
 
         return hits;
     }
