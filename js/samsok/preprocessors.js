449b1313a61c8f9e8e2d238e6b1b49d6a5f0caa8
Danni Efraim <danni@compiled.se>
2015-08-28 09:08:56 +0200

    Added comment to ArenaPreprocessor.

--- js/samsok/preprocessors.js
+++ js/samsok/preprocessors.js
@@ -3,6 +3,8 @@
 // The preprocessor must then execute the supplied function which triggers parsing (this is to enable
 // the preprocessors to work asynchronously).
 
+// Many Arena libraries will use a Javascript redirect to go directly to the book page if there's only one hit.
+// This preprocessor will detect and follow that redirect.
 var ArenaPreprocessor = function(provider, content, func) {
     var regex = /jQuery\(function\(a\)\{a\(document\)\.ready\(function\(\)\{location\.replace\("(.*?)"\)\}\)\}\)/g;
     var match = regex.exec(content);
