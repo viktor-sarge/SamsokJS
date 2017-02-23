6456a61690cd0f2ae99473b773197c68b37f5b6f
Viktor Sarge <viktor.sarge@regionhalland.se>
2017-02-23 14:33:28 +0100

    Replaced example file with the one we use in production

--- js/samsok/providers.js
+++ js/samsok/providers.js
@@ -51,85 +51,45 @@ App.Provider = Ember.Object.extend({
 
 var providers = [
     App.ProviderGroup.create({
-        groupName: 'Testgrupp',
+        groupName: 'Halland',
         providers: [
-            App.Provider.create({
-                parser: XsearchParser,
-                baseUrl: 'http://www.sverigesdepabibliotekochlanecentral.se/',
-                searchUrl: 'http://libris.kb.se/xsearch?query=@QUERY@%20AND%20bibl:umdp&format=json&holdings=true&n=200',
-                name: 'Depåbiblioteket i Umeå',
-                gotoUrl: 'http://libris.kb.se/hitlist?q=@QUERY@+bib%3aumdp&d=libris&m=10&p=1&s=r',
-                encoding: 'utf-8'
-            }),
-            App.Provider.create({
-                parser: SsbParser,
-                baseUrl: 'https://biblioteket.stockholm.se/',
-                searchUrl: 'http://biblioteket.stockholm.se/sok?freetext=',
-                name: 'Stockholms Stadsbibliotek',
-                encoding: 'utf-8'
-            }),
-            App.Provider.create({
-                parser: GotlibParser,
-                baseUrl: 'http://encore.gotlib.goteborg.se/',
-                searchUrl: 'http://encore.gotlib.goteborg.se/iii/encore/search/C__S@QUERY@__Orightresult__U1?lang=swe&suite=pearl',
-                name: 'Göteborg',
-                encoding: 'utf-8'
-            }),
-            App.Provider.create({
-                parser: MalmoParser,
-                preprocessor: BlockPhrasePreprocessorGenerator('Din sökfråga har blivit ändrad'),
-                baseUrl: 'http://malmo.stadsbibliotek.org/',
-                searchUrl: 'http://malmo.stadsbibliotek.org/search~S7*swe/?SUBMIT=Sök+%3E%3E%searchtype=X&searcharg=',
-                name: 'Malmö',
-                encoding: 'utf-8'
-            }),
-            App.Provider.create({
-                parser: OlaParser,
-                baseUrl: 'http://bibliotek.boras.se/',
-                searchUrl: 'http://bibliotek.boras.se/search?query=@QUERY@&searchtype=Biblioteket&sort=Relevance&fLibId=00000000-0000-0000-0000-000000000000',
-                name: 'Borås',
-                encoding: 'utf-8'
-            }),
-            App.Provider.create({
-                parser: KohaParser,
-                baseUrl: 'http://hylte.bibkat.se/',
-                searchUrl: 'http://hylte.bibkat.se/cgi-bin/koha/opac-search.pl?q=@QUERY@&branch_group_limit=',
-                name: 'Hylte',
-                encoding: 'utf-8'
-            }),
-            App.Provider.create({
-                parser: MinabibliotekParser,
-                baseUrl: 'http://www.minabibliotek.se/',
-                searchUrl: 'https://www.minabibliotek.se/search?query=@QUERY@&fMediaId=&fTarget=',
-                name: 'Minabibliotek',
-                encoding: 'utf-8'
-            })
-        ]
-    }),
-    App.ProviderGroup.create({
-        groupName: 'Libra',
-        providers: [
-            App.Provider.create({
-                parser: LibraParser,
-                baseUrl: 'http://opac.laholm.axiell.com/opac/',
-                searchUrl: 'http://opac.laholm.axiell.com/opac/search_result.aspx?TextFritext=',
-                name: 'Laholm',
-                encoding: 'utf-8'
-            }),
             App.Provider.create({
                 parser: LibraParser,
                 baseUrl: 'http://www5.falkenberg.se/opac/opac/',
                 searchUrl: 'http://www5.falkenberg.se/opac/opac/search_result.aspx?TextFritext=',
                 name: 'Falkenberg',
                 encoding: 'utf-8'
-            }),
+            }),        
             App.Provider.create({
-                parser: LibraParser,
-                baseUrl: 'http://bibold.kungsbacka.se/opac/',
-                searchUrl: 'http://bibold.kungsbacka.se/opac/search_result.aspx?TextFritext=',
+                parser: ArenaParser,
+                preprocessor: ArenaPreprocessor,
+                queryModifier: SpacetoPlusQueryModifier, 
+                baseUrl: '',
+                searchUrl: 'https://bibliotek.halmstad.se/web/arena/search?p_auth=g1Ro58aD&p_p_id=searchResult_WAR_arenaportlets&p_p_lifecycle=1&p_p_state=normal&p_p_mode=view&p_r_p_687834046_facet_queries=&p_r_p_687834046_sort_advice=field%3DRelevance%26direction%3DDescending&p_r_p_687834046_search_type=solr&p_r_p_687834046_search_query=',
+                name: 'Halmstad',
+                encoding: 'utf-8'
+            }),        
+             App.Provider.create({
+                parser: KohaParser,
+                baseUrl: 'http://hylte.bibkat.se/',
+                searchUrl: 'http://hylte.bibkat.se/cgi-bin/koha/opac-search.pl?q=@QUERY@&branch_group_limit=',
+                name: 'Hylte',
+                encoding: 'utf-8'
+            }),       
+            App.Provider.create({
+                parser: ArenaParser,
+                baseUrl: '',
+                searchUrl: 'https://bibliotek.kungsbacka.se/web/arena/search?p_auth=9zpfYNMD&p_p_id=searchResult_WAR_arenaportlets&p_p_lifecycle=1&p_p_state=normal&p_p_mode=view&p_r_p_687834046_facet_queries=&p_r_p_687834046_sort_advice=field%3DRelevance%26direction%3DDescending&p_r_p_687834046_search_type=solr&p_r_p_687834046_search_query=',
                 name: 'Kungsbacka',
                 encoding: 'utf-8'
             }),
+            App.Provider.create({
+                parser: LibraParser,
+                baseUrl: 'http://opac.laholm.axiell.com/opac/',
+                searchUrl: 'http://opac.laholm.axiell.com/opac/search_result.aspx?TextFritext=',
+                name: 'Laholm',
+                encoding: 'utf-8'
+            }),            
             App.Provider.create({
                 parser: LibraParser,
                 baseUrl: 'http://bib.varberg.se/opac/',
@@ -140,7 +100,20 @@ var providers = [
         ]
     }),
     App.ProviderGroup.create({
-        groupName: 'Mikromarc',
+        groupName: 'Lånecentralen',
+        providers: [
+            App.Provider.create({
+                parser: XsearchParser,
+                baseUrl: 'http://www.sverigesdepabibliotekochlanecentral.se/',
+                searchUrl: 'http://libris.kb.se/xsearch?query=@QUERY@%20AND%20bibl:umdp&format=json&holdings=true&n=200',
+                name: 'Depåbiblioteket i Umeå',
+                gotoUrl: 'http://libris.kb.se/hitlist?q=@QUERY@+bib%3aumdp&d=libris&m=10&p=1&s=r',
+                encoding: 'utf-8'
+            })        
+        ]
+    }),
+    App.ProviderGroup.create({
+        groupName: 'Andra regioner',
         providers: [
             App.Provider.create({
                 parser: MicroMarcParser,
@@ -150,23 +123,10 @@ var providers = [
                 encoding: 'utf-8'
             }),
             App.Provider.create({
-                parser: MicroMarcParser,
-                baseUrl: 'http://webbsok.mikromarc.se/Mikromarc3/web/',
-                searchUrl: 'http://webbsok.mikromarc.se/Mikromarc3/Web/search.aspx?Unit=6469&db=vargarda&SC=FT&SW=@QUERY@&LB=FT&IN=&SU=0&',
-                name: 'Vårgårda',
-                encoding: 'utf-8'
-            })
-        ]
-    }),
-    App.ProviderGroup.create({
-        groupName: 'Arena',
-        providers: [
-            App.Provider.create({
-                parser: ArenaParser,
-                preprocessor: ArenaPreprocessor, 
-                baseUrl: '',
-                searchUrl: 'http://bibliotek.mark.se/web/arena/search?p_p_state=normal&p_p_lifecycle=1&p_p_action=1&p_p_id=searchResult_WAR_arenaportlets&p_p_col_count=4&p_p_col_id=column-1&p_p_col_pos=1&p_p_mode=view&search_item_no=0&search_type=solr&search_query=',
-                name: 'Mark',
+                parser: OlaParser,
+                baseUrl: 'http://bibliotek.boras.se/',
+                searchUrl: 'http://bibliotek.boras.se/search?query=@QUERY@&searchtype=Biblioteket&sort=Relevance&fLibId=00000000-0000-0000-0000-000000000000',
+                name: 'Borås',
                 encoding: 'utf-8'
             }),
             App.Provider.create({
@@ -178,20 +138,10 @@ var providers = [
                 encoding: 'utf-8'
             }),
             App.Provider.create({
-                parser: ArenaParser,
-                preprocessor: ArenaPreprocessor, 
-                baseUrl: '',
-                searchUrl: 'http://biblioteken.vara.se/web/pub/search?p_p_state=normal&p_p_lifecycle=1&p_p_action=1&p_p_id=searchResult_WAR_arenaportlets&p_p_col_count=3&p_p_col_id=column-1&p_p_col_pos=1&p_p_mode=view&facet_queries=&search_item_no=0&search_type=solr&search_query=',
-                name: 'Vara',
-                encoding: 'utf-8'
-            }),
-            App.Provider.create({
-                parser: ArenaParser,
-                preprocessor: ArenaPreprocessor,
-                queryModifier: SpacetoPlusQueryModifier,
-                baseUrl: '',
-                searchUrl: 'http://bibliotek.halmstad.se/web/arena/search?p_p_state=normal&p_p_lifecycle=1&p_p_action=1&p_p_id=searchResult_WAR_arenaportlets&p_p_col_count=11&p_p_col_id=column-1&p_p_mode=view&search_item_no=0&search_type=solr&search_query=',
-                name: 'Halmstad',
+                parser: GotlibParser,
+                baseUrl: 'http://encore.gotlib.goteborg.se/',
+                searchUrl: 'http://encore.gotlib.goteborg.se/iii/encore/search/C__S@QUERY@__Orightresult__U1?lang=swe&suite=pearl',
+                name: 'Göteborg',
                 encoding: 'utf-8'
             }),
             App.Provider.create({
@@ -219,13 +169,57 @@ var providers = [
                 encoding: 'utf-8'
             }),
             App.Provider.create({
+                parser: MalmoParser,
+                baseUrl: 'http://malmo.stadsbibliotek.org/',
+                searchUrl: 'http://malmo.stadsbibliotek.org/search~S7*swe/?SUBMIT=Sök+%3E%3E%searchtype=X&searcharg=',
+                name: 'Malmö',
+                encoding: 'utf-8'
+            }),
+            App.Provider.create({
                 parser: ArenaParser,
                 preprocessor: ArenaPreprocessor, 
                 baseUrl: '',
-                searchUrl: 'http://bookitpub.bibliotekskno.se/web/pub/search?p_p_state=normal&p_p_lifecycle=1&p_p_action=1&p_p_id=searchResult_WAR_arenaportlets&p_p_col_count=3&p_p_col_id=column-1&p_p_col_pos=1&p_p_mode=view&facet_queries=&search_item_no=0&search_type=solr&search_query=',
-                name: 'SkåneNO',
+                searchUrl: 'http://bibliotek.mark.se/web/arena/search?p_p_state=normal&p_p_lifecycle=1&p_p_action=1&p_p_id=searchResult_WAR_arenaportlets&p_p_col_count=4&p_p_col_id=column-1&p_p_col_pos=1&p_p_mode=view&search_item_no=0&search_type=solr&search_query=',
+                name: 'Mark',
                 encoding: 'utf-8'
             }),
+            App.Provider.create({
+                parser: ArenaParser,
+                preprocessor: ArenaPreprocessor, 
+                baseUrl: '',
+                searchUrl: 'http://snokabibliotek.se/web/arena/search?p_p_state=normal&p_p_lifecycle=1&p_p_action=1&p_p_id=searchResult_WAR_arenaportlets&p_p_col_count=1&p_p_col_id=column-2&p_p_mode=view&facet_queries=&search_item_no=0&search_type=solr&search_query=',
+                name: 'SkåneNO',
+                encoding: 'utf-8'
+            }),     
+            App.Provider.create({
+                parser: SsbParser,
+                baseUrl: 'https://biblioteket.stockholm.se/',
+                searchUrl: 'http://biblioteket.stockholm.se/sok?freetext=',
+                name: 'Stockholms Stadsbibliotek',
+                encoding: 'utf-8'
+            }),
+            App.Provider.create({
+                parser: MinabibliotekParser,
+                baseUrl: 'http://www.minabibliotek.se/',
+                searchUrl: 'https://www.minabibliotek.se/search?query=@QUERY@&fMediaId=&fTarget=',
+                name: 'Umeå - Mina bibliotek',
+                encoding: 'utf-8'
+            }),
+            App.Provider.create({
+                parser: ArenaParser,
+                preprocessor: ArenaPreprocessor, 
+                baseUrl: '',
+                searchUrl: 'http://biblioteken.vara.se/web/pub/search?p_p_state=normal&p_p_lifecycle=1&p_p_action=1&p_p_id=searchResult_WAR_arenaportlets&p_p_col_count=3&p_p_col_id=column-1&p_p_col_pos=1&p_p_mode=view&facet_queries=&search_item_no=0&search_type=solr&search_query=',
+                name: 'Vara',
+                encoding: 'utf-8'
+            }),
+            App.Provider.create({
+                parser: MicroMarcParser,
+                baseUrl: 'http://webbsok.mikromarc.se/Mikromarc3/web/',
+                searchUrl: 'http://webbsok.mikromarc.se/Mikromarc3/Web/search.aspx?Unit=6469&db=vargarda&SC=FT&SW=@QUERY@&LB=FT&IN=&SU=0&',
+                name: 'Vårgårda',
+                encoding: 'utf-8'
+            }),  
             App.Provider.create({
                 parser: ArenaParser,
                 preprocessor: ArenaPreprocessor, 
@@ -233,7 +227,7 @@ var providers = [
                 searchUrl: 'http://bibliotek.vaxjo.se/web/arena/search?p_p_state=normal&p_p_lifecycle=1&p_p_action=1&p_p_id=searchResult_WAR_arenaportlets&p_p_col_count=4&p_p_col_id=column-1&p_p_col_pos=2&p_p_mode=view&facet_queries=&search_item_no=0&search_type=solr&search_query=',
                 name: 'Växjö',
                 encoding: 'utf-8'
-            })
+            })  
         ]
     })
 ];
